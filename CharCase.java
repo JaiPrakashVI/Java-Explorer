d5af7d7f7e3dac497086f4c713b3f40d68942047
JaiPrakashVI <jaiprakashviofficial@gmail.com>
2026-04-09 00:53:58 +0530

    Add comment for character input in CharCase
    Added comment to explain character input handling.

--- CharCase.java
+++ CharCase.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class CharCase {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        char ch = sc.next().trim().charAt(0);
+        char ch = sc.next().trim().charAt(0);    // trim removes the extra spaces here & charAt returns the char in the particular index of the string
         if (ch >= 'a' && ch<= 'z'){
             System.out.println("Lowercase");
         } else {
