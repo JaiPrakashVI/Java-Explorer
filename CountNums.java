d4115ccd7ccb66120b9ff64d61018a5839abe89a
JaiPrakashVI <jaiprakashviofficial@gmail.com>
2026-04-11 12:52:03 +0530

    Add CountNums.java to count target digits in a number

---
+++ CountNums.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Count_num {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int target = sc.nextInt();
+        int count = 0;
+        int rem =0;
+        while (n>0){
+            rem = n%10;
+            if (rem == target){
+                count ++;
+            }
+            n = n / 10; // to remove the last digit.
+        }
+        System.out.println(count);
+    }
+}
