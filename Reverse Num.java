0bf70f24ff5ea52e68344d71467ad34e4f4cb78d
JaiPrakashVI <jaiprakashviofficial@gmail.com>
2026-04-11 13:47:51 +0530

    Modify reverse program for continuous input
    Refactor to allow multiple reversals without restarting the program.

--- Reverse Num.java
+++ Reverse Num.java
@@ -2,16 +2,21 @@ import java.util.Scanner;
 
 public class reverse {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);{
-            int n = sc.nextInt();
-            int rev =0;
-            while(n>0){
-                int rem = n%10;
-                rev = rev * 10 + rem;
-                n/=10;
-               // System.out.println(rev);          #reverse pattern 
+        Scanner sc = new Scanner(System.in);
+            while (true) { // just to run the while loop inside this for multiple times without running the whole program again
+                int n = sc.nextInt();
+                int rev = 0;
+                while (true) {
+                    while (n > 0) {
+                        int rem = n % 10;
+                        rev = rev * 10 + rem;
+                        n /= 10;
+                        // System.out.println(rev); #reverse pattern
+                    }
+                    System.out.println(rev); // complete reverse string
+                    break;
+                }
             }
-            System.out.println(rev); // complete reverse string
         }
     }
-}
+
